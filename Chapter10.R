2b5d85c8e1e7b9bc27e1b77469e19a97ebc234d5
zimancheng <46982457+zimancheng@users.noreply.github.com>
2020-11-10 20:11:34 +0800

    Add files via upload

--- Chapter10.R
+++ Chapter10.R
@@ -79,3 +79,62 @@ dd <- as.dist(1 - cor(t(x)))
 plot(hclust(dd, method = "complete"), main = "Complete Linkage with Correlation-Based Distance", xlab = "", ylab = "")
 
 #Lab3: NCI60 Data
+library(ISLR)
+nci.labs <- NCI60$labs
+nci.data <- NCI60$data
+dim(nci.data)
+length(nci.labs)
+
+table(nci.labs)
+
+#PCA on the NCI60 Data
+pr.out <- prcomp(nci.data, scale = TRUE)
+
+Cols <- function(vec) {
+  cols <- rainbow(length(unique(vec)))
+  return(cols[as.numeric(as.factor(vec))])
+}
+
+par(mfrow = c(1, 2))
+plot(pr.out$x[, 1:2], col = Cols(nci.labs), pch = 19, xlab = "Z1", ylab = "Z2")
+plot(pr.out$x[,c(1, 3)], col = Cols(nci.labs), pch = 19, xlab = "Z1", ylab = "Z3")
+summary(pr.out)
+
+plot(pr.out)
+pve <- 100*pr.out$sdev^2/sum(pr.out$sdev^2)
+par(mfrow = c(1,2))
+plot(pve, type = "o", ylab = "PVE", xlab = "Principal Component", col = "blue")
+plot(cumsum(pve), type = "o", ylab = "Cumulative PVE", xlab = "Principal Component", col = "Blue")
+
+plot(summary(pr.out)$importance[2, ])
+plot(summary(pr.out)$importance[3,])
+
+#Clustering on NCI60 Data
+sd.data <- scale(nci.data)
+par(mfrow = c(1, 3))
+data.dist <- dist(sd.data)
+
+plot(hclust(data.dist), labels = nci.labs, main = "Complete Linkage", xlab = "", ylab = "")
+plot(hclust(data.dist, method = "average"), labels = nci.labs, main = "Average Linkage", xlab = "", ylab = "")
+plot(hclust(data.dist, method = "single"), labels = nci.labs, main = "Single Linkage", xlab = "", ylab = "")
+
+hc.out <- hclust(dist(sd.data))
+hc.cluster <- cutree(hc.out, 4)
+table(hc.cluster, nci.labs)
+
+par(mfrow = c(1, 1))
+plot(hc.out, labels=nci.labs)
+abline(h=139, col = "red")
+
+hc.out
+
+#Kmeans on NCI60
+set.seed(2)
+km.out <- kmeans(sd.data, 4, nstart = 20)
+km.clusters <- km.out$cluster
+table(km.clusters, hc.cluster)
+
+hc.out <- hclust(dist(pr.out$x[, 1:5]))
+plot(hc.out, labels = nci.labs, main = "Hier.Clust. on First Five Score Vectors")
+table(cutree(hc.out, 4), nci.labs)
+
